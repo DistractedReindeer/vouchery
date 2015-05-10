2b45647d32fc3f5a7f4ac471327e8acd19fce1ac
Cheng Tian <chent9288@Cheng-MacBook-Pro.local>
2015-05-10 14:42:03 -0700

    added webpack to gruntfile

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './app/app.jsx'
+    './frontend/frontend/app/app.jsx'
   ],
   output: {
     path: __dirname + '/assets/js/',
