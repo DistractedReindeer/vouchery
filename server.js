c91f14a632fe3e9c2b2ef3189845e7a4ad0d8c18
Darren Wong <darrenwong23@gmail.com>
2015-05-09 20:27:26 -0700

    changed process.env to uppercase

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var app = require('./server/server-config.js');
 var port = process.env.PORT || 4568;
-
+console.log(process.env);
+console.log(process.env.ClientID);
+console.log(process.env.ClientSecret);
 app.listen(port);
 console.log('Server now listening on port ' + port);
